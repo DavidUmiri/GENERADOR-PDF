9410996255736059612f137b3c0a067074ec7206
DavidUmiri <davidumiricondo@gmail.com>
2023-10-17 15:21:23 +0200

    Commit inicial

---
+++ src/main/java/demo/generadorpdf2/Main.java
@@ -0,0 +1,16 @@
+package demo.generadorpdf2;
+
+import demo.generadorpdf2.gui.Principal;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Llamamos y hacemos visible a nuestra GUI
+        Principal principal = new Principal();
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+
+    }
+
+}
